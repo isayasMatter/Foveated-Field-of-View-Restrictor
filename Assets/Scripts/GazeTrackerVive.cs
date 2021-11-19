2d6d08ea044ba6d9b0e89ebc1c365d8fb639fabe
Isayas Adhanom <isayasadhanom@gmail.com>
2021-11-18 22:34:22 -0800

    Added sample GIF.

--- Assets/Scripts/GazeTrackerVive.cs
+++ Assets/Scripts/GazeTrackerVive.cs
@@ -1,5 +1,4 @@
-﻿//========= Copyright 2018, HTC Corporation. All rights reserved. ===========
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections.Generic;
 
 
@@ -9,30 +8,30 @@ public class GazeTrackerVive : MonoBehaviour
     public static float gaze_x;
 	public static float gaze_y;
     
-    public float smoothTime = 0.15f;
-	private float _avSlewX;
-	private float _avSlewY;
-
-    private float _tunnel_x;
-	private float _tunnel_y;
-
-    public float gx=0.0f,gy=0.0f;
+        
     private List<float> gazeArrayX = new List<float>();
     float gazeAverageX = 0.0F;
 
     private List<float> gazeArrayY = new List<float>();
     float gazeAverageY = 0.0f;
 
-    float raw_x = 0.0f, raw_y = 0.0f;
-
+    // The size of the sliding window for the runnoing gaze smoother
 	public float smoothingWindow = 10;
 
     //Initialization done once:
     float lowPassFactor = 0.8f; //Value should be between 0.01f and 0.99f. Smaller value is more damping.
     bool init = true;
+
+    //Buffer for the intermediate value of the low pass filter
     Vector2 intermediateValueBuf;
 
 
+    public float smoothTime = 0.15f;
+    private float _avSlewX;
+    private float _avSlewY;
+
+    private float _tunnel_x;
+    private float _tunnel_y;
 
     private void Start()
     {        
@@ -51,8 +50,8 @@ public class GazeTrackerVive : MonoBehaviour
         // Smooth the signal with running average smoothing or smooth damping to remove sudden spikes.
         Vector2 smoothedGaze = 
           
-        Sigtrap.ImageEffects.TunnellingDynamic.offsetx = gx;
-		Sigtrap.ImageEffects.TunnellingDynamic.offsety = gy;
+        Sigtrap.ImageEffects.TunnellingDynamic.offsetx = smoothedGaze.x;
+		Sigtrap.ImageEffects.TunnellingDynamic.offsety = smoothedGaze.y;
             
 
             
